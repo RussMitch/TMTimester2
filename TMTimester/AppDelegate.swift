87fb6e9f34a2678ef9fe8418ce505f69f7321b03
Russell <russell@russell-research.com>
2015-12-22 10:54:52 -0500

    updates

--- TMTimester/AppDelegate.swift
+++ TMTimester/AppDelegate.swift
@@ -8,6 +8,8 @@
 //------------------------------------------------------------------------------
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 
@@ -19,6 +21,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
     //------------------------------------------------------------------------------
     {
+        Parse.enableLocalDatastore()
+        
+        Parse.setApplicationId("1Cpum25gZqxdQ8hYAC38vL61QrQPI8H0srazNVzo", clientKey: "Y0TvIY0s5F0wzD1WvbdURrmPo7psOjLV367Ekdmu")
+        
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 }
